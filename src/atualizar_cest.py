ba6557317eeed75690a142e2f9ff96150be84288
Willian Cruz <willianalbertocruz@gmail.com>
2024-06-13 00:56:54 -0300

    Alterações na estrutura do banco de dados
    1. Agora os NCMs vinculados ao CEST estão armazenados em uma tabela separada das descrições de cada NCM.
    2. Alterações na estrutura do JSON que contém os CEST para permitir uma atualização mais fluida quando necessário.
    3. Correção de pequenos erros.

--- src/atualizar_cest.py
+++ src/atualizar_cest.py
@@ -5,26 +5,35 @@ conn = sqlite3.connect('fisco.sqlite')
 cursor = conn.cursor()
 
 f = open('lista_cest.json', encoding='utf-8')
-data = json.load(f)
+dados = json.load(f)
 
-cursor.execute("""DELETE FROM cest_codigos""")
-conn.commit()
+cursor.execute("""DELETE FROM cest_descricoes""")
+cursor.execute("""DELETE FROM cest_vinc_ncm""")
 
 try:
-    for i in data:
-        item = str(i["ITEM"]).replace(' ','')
-        cest = str(i['CEST']).replace(' ','')
-        ncm = str(i['NCM']).replace(' ','').replace('.','').split('|')
-        desc = i['DESCRIÇÃO']
-        exce = i['EXCEÇÕES']
-        revo = int(i['REVOGADO'])
-        segm = int(cest[:2])
+    for anexo in dados:
+        dados_anexo = dados.get(anexo)
 
-        for n in ncm:
-            conn.execute("""INSERT INTO cest_codigos (revogado, cod_item, cod_cest, cod_ncm, fk_id_segmento, descricao_cest) VALUES (?,?,?,?,?,?)""",
-                    (revo, item, cest, n, segm, desc))
+        for i in dados_anexo:
+            item = str(i['ITEM']).replace(' ', '')
+
+            item = str(i['ITEM']).replace(' ', '')
+            cest = str(i['CEST']).replace(' ', '').replace('.', '')
+            ncm = str(i['NCM']).replace(' ', '').replace('.', '').split('|')
+            desc = i['DESCRIÇÃO']
+            revo = int(i['REVOGADO'])
+            segm = int(cest[:2])
+
+            conn.execute('INSERT INTO cest_descricoes (cod_item, cod_cest, '
+                        'fk_id_segmento, descricao_cest) VALUES (?,?,?,?)',
+                        (item, cest, segm, desc))
+            
+            for n in ncm:
+                conn.execute('INSERT INTO cest_vinc_ncm (fk_cod_cest, cod_ncm, '
+                             'revogado) VALUES (?,?,?)', (cest, n, revo))
+
 except sqlite3.IntegrityError as e:
-    print (e)
+    print(e)
     conn.rollback()
-        
+
 conn.commit()
--- src/pesquisa.py
+++ src/pesquisa.py
@@ -2,7 +2,7 @@ import sqlite3
 from os import system, name
 from beautifultable import BeautifulTable
 
-conn = sqlite3.connect('src/fisco.sqlite')
+conn = sqlite3.connect('fisco.sqlite')
 cursor = conn.cursor()
 
 
@@ -24,22 +24,24 @@ def formatar_ncm(ncm: str) -> str:
         return f'{ncm[:4]}.{ncm[4:6]}.{ncm[6:]}'
 
 
+def formatar_cest(cest: str) -> str:
+    return f'{cest[0:2]}.{cest[2:5]}.{cest[5:]}'
+
+
 def pesquisar_cest(ncm: str) -> list:
-    DQL_CEST = """SELECT a.cod_item, 
-                        a.cod_cest, 
-                        a.descricao_cest, 
-                        b.descricao_segmento 
-                FROM cest_codigos a 
-                    JOIN cest_segmentos b 
-                    ON a.fk_id_segmento=b.id_segmento
-                WHERE (cod_ncm IS ? OR 
-                        cod_ncm IS ? OR 
-                        cod_ncm IS ? OR 
-                        cod_ncm IS ? OR 
-                        cod_ncm IS ? OR 
-                        cod_ncm IS ?) 
-                        AND revogado IS '0'
-               """
+    DQL_CEST = """SELECT b.cod_item, 
+                         b.cod_cest, 
+                         b.descricao_cest, 
+                         c.descricao_segmento 
+                  FROM cest_vinc_ncm a
+                  JOIN cest_descricoes b ON a.fk_cod_cest = b.cod_cest
+                  JOIN cest_segmentos c ON b.fk_id_segmento = c.id_segmento
+                  WHERE (a.cod_ncm = ? OR
+                        a.cod_ncm = ? OR
+                        a.cod_ncm = ? OR
+                        a.cod_ncm = ? OR
+                        a.cod_ncm = ? OR
+                        a.cod_ncm = ?) AND revogado = 0"""
 
     dados = cursor.execute(DQL_CEST, (ncm[:2], ncm[:4], ncm[:5], ncm[:6],
                                       ncm[:7], ncm)).fetchall()
@@ -85,13 +87,13 @@ tabela.set_style(BeautifulTable.STYLE_COMPACT)
 
 while True:
     tabela.clear()
-    ncm = input('Digite o NCM/SH a procurar (0 para sair): ')
+    ncm = input('\nDigite o NCM/SH a procurar (0 para sair): ')
     limpar_tela()
 
     if ncm == '0':
         exit()
     elif len(ncm) < 8 or not ncm.isnumeric():
-        print('NCM/SH Inválido! (incompleto ou contém letras/pontos)\n')
+        print('NCM/SH Inválido! (incompleto ou contém letras/pontos)')
     else:
         if pesquisar_ncm(ncm):  # verifica se o NCM/SH existe na base
             dados_cest = pesquisar_cest(ncm)
@@ -104,7 +106,7 @@ while True:
                 print('\n')
 
                 for i in dados_cest:
-                    tabela.rows.append([i[0], i[1], i[3], i[2]])
+                    tabela.rows.append([i[0], formatar_cest(i[1]), i[3], i[2]])
 
                 print(tabela)
             else:
@@ -123,4 +125,4 @@ while True:
                                     'itens deste anexo.'])
                 print(tabela)
         else:
-            print("* NCM/SH não encontrado na tabela da Siscomex * \n")
+            print("* NCM/SH não encontrado na tabela da Siscomex *")
